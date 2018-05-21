eca0d5e1705b2f56e0f89b68ff92875ba45789d9
郭静茹 <1228738834@qq.com>
2018-05-21 16:58:14 +0800

    gghghf

--- src/components/pages/children/StateChild.vue
+++ src/components/pages/children/StateChild.vue
@@ -1,16 +1,40 @@
 <template>
-  <div>动态子页面</div>
+  <div class="stateChild">
+    <span v-for="(stateImg, index) in stateImgList" :key="index" :style="stateImg ? { 'background-image':'url(' + stateImg.stateImg + ' ) ' } : ''" ></span>
+  </div>
 </template>
 
 <script>
 export default {
   name: 'child-state',
   data () {
-    return {}
+    return {
+      stateImgList: [
+        { stateImg: 'http://img.zcool.cn/community/01f9df56fccb6c32f875a944518d4c.JPG' },
+        { stateImg: 'http://img.zcool.cn/community/01f9df56fccb6c32f875a944518d4c.JPG' },
+        { stateImg: 'http://img.zcool.cn/community/01f9df56fccb6c32f875a944518d4c.JPG' },
+        { stateImg: 'http://img.zcool.cn/community/01f9df56fccb6c32f875a944518d4c.JPG' }
+      ]
+    }
   }
 }
 </script>
 
 <style scoped>
-
+.stateChild{
+  width: 100%;
+  display: flex;
+  flex-wrap: wrap;
+  flex-direction: row;
+  justify-content: flex-start;
+}
+.stateChild>span{
+  display: inline-block;
+  width: calc(100% / 3);
+  height: 0;
+  padding-bottom: calc(100% / 3 + 50px);
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: auto 100%;
+}
 </style>
--- src/components/pages/children/VideoChild.vue
+++ src/components/pages/children/VideoChild.vue
@@ -1,16 +1,40 @@
 <template>
-  <div>视频子页面</div>
+  <div class="video-child" >
+    <span v-for="(videoImg, index) in videoImgList" :key="index" :style="videoImg ? { 'background-image':'url(' + videoImg.videoImg + ' ) ' } : ''" ></span>
+  </div>
 </template>
 
 <script>
 export default {
   name: 'child-video',
   data () {
-    return {}
+    return {
+      videoImgList: [
+        { videoImg: 'http://pic.58pic.com/58pic/14/91/93/83M58PICPv5_1024.jpg' },
+        { videoImg: 'http://img.zcool.cn/community/010f5c586df97ba8012060c8656bf3.jpg@900w_1l_2o_100sh.jpg' },
+        { videoImg: 'http://pic.58pic.com/58pic/14/91/93/83M58PICPv5_1024.jpg' },
+        { videoImg: 'http://pic.58pic.com/58pic/14/91/93/83M58PICPv5_1024.jpg' }
+      ]
+    }
   }
 }
 </script>
 
 <style scoped>
-
+.video-child{
+  width: 100%;
+  display: flex;
+  flex-wrap: wrap;
+  flex-direction: row;
+  justify-content: flex-start;
+}
+.video-child>span{
+  display: inline-block;
+  width: calc(100% / 3);
+  height: 0;
+  padding-bottom: calc(100% / 3 + 50px);
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: auto 100%;
+}
 </style>
